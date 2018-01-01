b8dd12243d3ac1384662c9530292024db70550bc
Ralf S. Engelschall <rse@engelschall.com>
2018-01-01 11:19:35 +0100

    bump year in copyright messages

--- README.md
+++ README.md
@@ -19,6 +19,6 @@ It provides just the CLI framework. The particular functionalities are provided 
 Copyright &amp; License
 -----------------------
 
-Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Copyright &copy; 2016-2018 [Gemstone Project](http://gemstonejs.com)<br/>
 Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
 
--- bin/gemstone-tool-cli.js
+++ bin/gemstone-tool-cli.js
@@ -29,7 +29,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 /*
 **  GemstoneJS -- Gemstone JavaScript Technology Stack
-**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Copyright (c) 2016-2018 Gemstone Project <http://gemstonejs.com>
 **  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
 */
 
