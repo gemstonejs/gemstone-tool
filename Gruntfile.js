260fce65dee7219c8d16905c82a9631add0ae2ea
Ralf S. Engelschall <rse@engelschall.com>
2018-02-16 22:28:13 +0100

    switch to babel-preset-env

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
         },
         babel: {
             options: {
-                presets: [ "es2015", "es2016", "es2017", "stage-3", "stage-2" ],
+                presets: [ [ "env", { "targets": { "node": "7.6" } } ] ],
                 plugins: [ "transform-runtime" ]
             },
             "gemstone-tool-cli": {
